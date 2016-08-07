bfbc7cb1eb66226c037bbba4febe0bb6b1970d00
David Kimura <dkimura@pivotal.io>
2016-08-07 10:06:43 -0700

    Add operations file

---
+++ src/operations.cpp
@@ -0,0 +1,23 @@
+#include "operations.hpp"
+
+void
+Delete(File f)
+{
+}
+
+
+void
+Read(File f)
+{
+}
+
+void
+Update(File f)
+{
+}
+
+
+void
+Write(File f)
+{
+}
---
+++ src/operations.hpp
@@ -0,0 +1,10 @@
+#include "files.hpp"
+
+
+void Delete(File f);
+
+void Read(File f);
+
+void Update(File f);
+
+void Write(File f);
