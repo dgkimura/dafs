c15275bd9aed887f4132eba61ece9c7425834ebd
David Kimura <dkimura@pivotal.io>
2016-09-03 22:41:29 -0700

    Add server methods

--- src/server.cpp
+++ src/server.cpp
@@ -3,4 +3,63 @@
 
 namespace dafs
 {
+    using boost::asio::ip::tcp;
+
+
+    Server::Server(Storage storage_, std::string address, short port)
+        : io_service_(),
+          acceptor_(io_service_, tcp::endpoint(tcp::v4(), port)),
+          socket_(io_service_),
+          storage(storage_)
+    {
+    }
+
+
+    Server::~Server()
+    {
+        io_service_.stop();
+        acceptor_.close();
+    }
+
+
+    void
+    Server::do_accept()
+    {
+        acceptor_.async_accept(socket_,
+            [this](boost::system::error_code ec_accept)
+            {
+                if (!ec_accept)
+                {
+                    std::make_shared<Session>(std::move(socket_))->Start();
+                }
+                do_accept();
+            });
+    }
+
+
+    Server::Session::Session(
+        boost::asio::ip::tcp::socket socket
+    )
+        : socket_(std::move(socket))
+    {
+    }
+
+
+    void
+    Server::Session::Start()
+    {
+        auto self(shared_from_this());
+        socket_.async_read_some(boost::asio::buffer(data_, max_length),
+            [this, self](boost::system::error_code ec, std::size_t length)
+            {
+                if (!ec)
+                {
+                    // 1. Deserialize std::string(data_)
+                    // 2. Read request
+                    // 3. Find block
+                    // 4. Return response
+                }
+            }
+        );
+    }
 }
--- src/server.hpp
+++ src/server.hpp
@@ -1,9 +1,47 @@
+#include <boost/asio.hpp>
+
 #include "filesystem.hpp"
+#include "storage.hpp"
 
 
 namespace dafs
 {
     class Server
     {
+    public:
+
+        Server(Storage storage, std::string address, short port);
+
+        ~Server();
+
+    private:
+
+        void do_accept();
+
+        boost::asio::io_service io_service_;
+
+        boost::asio::ip::tcp::acceptor acceptor_;
+
+        boost::asio::ip::tcp::socket socket_;
+
+        Storage storage;
+
+        class Session : public std::enable_shared_from_this<Session>
+        {
+        public:
+            Session(
+                boost::asio::ip::tcp::socket socket
+            );
+
+            void Start();
+
+        private:
+
+            boost::asio::ip::tcp::socket socket_;
+
+            enum {max_length = 8192};
+
+            char data_[max_length];
+        };
     };
 }
