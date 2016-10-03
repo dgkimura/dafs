531a3b935465b7e566acfeff5e0ac7fdd1742415
David Kimura <dkimura@pivotal.io>
2016-10-02 17:42:58 -0700

    Add delta link into diflib interface

--- src/delta.cpp
+++ src/delta.cpp
@@ -1,6 +1,7 @@
 #include "diflib.h"
 
 #include "delta.hpp"
+#include "filesystem.hpp"
 
 
 namespace dafs
@@ -8,10 +9,21 @@ namespace dafs
     Delta
     DeltaFactory::Create(std::string filename, std::string was, std::string is)
     {
+        char edit_script[BLOCK_SIZE_IN_BYTES];
+        int edit_script_length = BLOCK_SIZE_IN_BYTES;
+
+        int error = ComputeEditScript(
+            &was[0],
+            was.length(),
+            &is[0],
+            is.length(),
+            edit_script,
+            edit_script_length
+        );
         Delta delta
         {
             filename,
-            ""
+            std::string(edit_script)
         };
         return delta;
     }
