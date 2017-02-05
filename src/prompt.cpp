089c04ee412680c11637e106ada8878d5f4a4b23
David Kimura <dkimura@pivotal.io>
2017-02-05 13:13:51 -0800

    Add linkage to dafs-cli

--- src/prompt.cpp
+++ src/prompt.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <vector>
+
+#include "dafs/messages.hpp"
+#include "dafs/sender.hpp"
 
 
 int main(void)
 {
-    std::cout << "dafs> ";
     std::string input;
-    std::cin >> input;
+
+    while (input != "quit")
+    {
+        std::cout << "dafs> ";
+        std::cin >> input;
+
+        dafs::Message m
+        {
+            "127.0.0.1:9000",       // from
+            "127.0.0.1:9000",       // to
+            "my content",
+            dafs::MessageType::WriteBlock,
+            std::vector<dafs::MetaData>()
+        };
+
+        dafs::NetworkSender sender;
+        sender.Reply(m);
+    }
 }
