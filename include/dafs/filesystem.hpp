9a9001603d0d5ff2e28d0c4584fe27e692b0694b
David Kimura <dkimura@pivotal.io>
2017-06-11 13:26:17 -0700

    Extract identity object into standalone file

--- include/dafs/filesystem.hpp
+++ include/dafs/filesystem.hpp
@@ -1,16 +1,11 @@
 #pragma once
 
-#include <map>
 #include <limits>
 #include <sstream>
-
-#include <boost/filesystem.hpp>
-#include <boost/uuid/uuid_generators.hpp>
-#include <boost/uuid/uuid.hpp>
-#include <boost/uuid/uuid_io.hpp>
-
 #include <string>
 #include <vector>
+
+#include "dafs/identity.hpp"
 
 
 namespace dafs
@@ -39,88 +34,6 @@ namespace dafs
     struct Bytes
     {
         char contents[BLOCK_SIZE_IN_BYTES];
-    };
-
-
-    //
-    // Associates stored objects with partitions.
-    //
-    struct Identity
-    {
-        boost::uuids::string_generator gen;
-
-        std::string id;
-
-        Identity()
-        {
-            id = "00000000-0000-0000-0000-000000000000";
-        }
-
-        Identity(std::string uuid)
-        {
-            id = uuid;
-        }
-
-        bool
-        operator==(const Identity& rhs) const
-        {
-            return gen(id) == gen(rhs.id);
-        }
-
-        bool
-        operator!=(const Identity& rhs) const
-        {
-            return gen(id) != gen(rhs.id);
-        }
-
-        bool
-        operator<(const Identity& rhs) const
-        {
-            return gen(id) < gen(rhs.id);
-        }
-
-        bool
-        operator>(const Identity& rhs) const
-        {
-            return gen(id) > gen(rhs.id);
-        }
-
-        bool
-        operator<=(const Identity& rhs) const
-        {
-            return gen(id) <= gen(rhs.id);
-        }
-
-        bool
-        operator>=(const Identity& rhs) const
-        {
-            return gen(id) >= gen(rhs.id);
-        }
-
-        Identity&
-        operator+=(int value)
-        {
-            boost::uuids::uuid raw_id = gen(id);
-
-            unsigned char uuid_data[raw_id.size()];
-            std::memcpy(&uuid_data, &raw_id, raw_id.size());
-
-            unsigned char carry = 0;
-            for (int i=raw_id.size()-1; i>0; i--)
-            {
-                auto max = std::numeric_limits<unsigned char>::max();
-                int updated = uuid_data[i] + carry + (max & value);
-                uuid_data[i] = updated;
-                carry = (updated > max ? 1 : 0);
-                value = value >> std::numeric_limits<unsigned char>::digits;
-            }
-
-            boost::uuids::uuid u;
-            std::memcpy(&u, uuid_data, raw_id.size());
-            id = boost::uuids::to_string(u);
-
-            return *this;
-        }
     };
 
 
