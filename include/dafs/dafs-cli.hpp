cee42c011ccc574c503229e8a7afcd2f633c868e
David Kimura <dkimura@pivotal.io>
2017-06-14 09:08:17 -0700

    Add filesystem structures

--- include/dafs/dafs-cli.hpp
+++ include/dafs/dafs-cli.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
+#include <array>
 #include <functional>
 #include <unordered_map>
 #include <vector>
 
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
 #include "dafs/messages.hpp"
+#include "dafs/identity.hpp"
 
 
 namespace dafs
@@ -15,9 +20,95 @@ namespace dafs
     };
 
 
-    void GetNodeDetails(
-        dafs::Address address,
-        std::vector<std::string> args);
+    struct SuperBlock
+    {
+        dafs::Identity filemap;
+    };
+
+
+    struct FileMap
+    {
+        dafs::Identity next;
+
+        std::unordered_map<std::string, dafs::Identity> files;
+    };
+
+
+    struct File
+    {
+        std::string filename;
+
+        std::array<dafs::Identity, 100> blocks;
+
+        dafs::Identity extended;
+    };
+
+
+    template <typename Archive>
+    void serialize(Archive& ar, dafs::SuperBlock& obj, const unsigned int version)
+    {
+        ar & obj.filemap;
+    }
+
+
+    template <typename Archive>
+    void serialize(Archive& ar, dafs::FileMap& obj, const unsigned int version)
+    {
+        ar & obj.next;
+        int size;
+        if (Archive::is_loading::value)
+        {
+            ar & size;
+            for (int i=0; i<size; i++)
+            {
+                std::string filename;
+                ar & filename;
+                dafs::Identity identity;
+                ar & identity;
+
+                obj.files[filename] = identity;
+            }
+        }
+        else
+        {
+            size = obj.files.size();
+            ar & size;
+            for (auto i : obj.files)
+            {
+                ar & i.first;
+                ar & i.second;
+            }
+        }
+    }
+
+
+    template <typename Archive>
+    void serialize(Archive& ar, dafs::File& obj, const unsigned int version)
+    {
+        ar & obj.filename;
+        int size;
+
+        if (Archive::is_loading::value)
+        {
+            ar & size;
+            for (int i=0; i<size; i++)
+            {
+                dafs::Identity identity;
+                ar & identity;
+                obj.blocks[i] = identity;
+            }
+        }
+        else
+        {
+            size = obj.blocks.size();
+            ar & size;
+            for (int i=0; i<size; i++)
+            {
+                ar & obj.blocks[i];
+            }
+        }
+        ar & obj.extended;
+    }
 
 
     void UploadFile(
@@ -26,6 +117,11 @@ namespace dafs
 
 
     void DownloadFile(
+        dafs::Address address,
+        std::vector<std::string> args);
+
+
+    void GetNodeDetails(
         dafs::Address address,
         std::vector<std::string> args);
 
@@ -65,9 +161,9 @@ namespace dafs
         std::function<void(dafs::Address address, std::vector<std::string>)>
     > commands =
     {
-        { "info", dafs::GetNodeDetails },
         { "upload", dafs::UploadFile },
         { "download", dafs::DownloadFile },
+        { "info", dafs::GetNodeDetails },
         { "join", dafs::AddNodeToCluster },
         { "exit", dafs::RemoveNodeFromCluster },
         { "ab", dafs::ExecuteAllocateBlock },
