9c7a8cab822f4a5abec0f48d5cec7ff343efe762
David Kimura <dkimura@pivotal.io>
2017-06-26 08:55:46 -0700

    Add signal unittest

---
+++ unittests/signal_unittest.cpp
@@ -0,0 +1,20 @@
+#include <functional>
+#include <thread>
+
+#include <gtest/gtest.h>
+
+#include "dafs/signal.hpp"
+
+
+TEST(SignalTest, testSetResultAndWait)
+{
+    dafs::Signal s;
+    dafs::Result actual, expected{true, "the_contents"};
+
+    std::thread t([&s, &actual](){ actual = s.Wait(); });
+    s.Set(expected);
+    t.join();
+
+    ASSERT_EQ(expected.success, actual.success);
+    ASSERT_EQ(expected.content, actual.content);
+}
