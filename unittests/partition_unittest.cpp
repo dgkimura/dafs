993df36e9306a5708e980bfc30fef8450bcfc446
David Kimura <dkimura@pivotal.io>
2017-07-08 11:48:56 -0700

    Add partition unittest for get nodeset details

--- unittests/partition_unittest.cpp
+++ unittests/partition_unittest.cpp
@@ -57,3 +57,61 @@ TEST(PartitionTest, testGetIdentityWillParseDetailsFile)
     auto identity = partition.GetIdentity();
     ASSERT_EQ("22222222-2222-2222-2222-222222222222", identity.id);
 }
+
+
+TEST(PartitionTest, testGetNodeSetDetailsWillParseDetailsFile)
+{
+    auto ping = std::unique_ptr<MockPing>(new MockPing());
+    auto storage = std::shared_ptr<MockStorage>(new MockStorage());
+    auto info = dafs::BlockInfo { Constant::DetailsName, dafs::Identity() };
+    auto format = dafs::BlockFormat {
+        dafs::Serialize(
+            dafs::ReplicatedEndpoints {
+                dafs::Endpoint {
+                    dafs::Address{"1.0.0.0", 100},
+                    dafs::Address{"1.0.0.1", 101},
+                    dafs::Identity("11111111-1111-1111-1111-111111111111") },
+                dafs::Endpoint {
+                    dafs::Address{"2.0.0.0", 200},
+                    dafs::Address{"2.0.0.1", 201},
+                    dafs::Identity("22222222-2222-2222-2222-222222222222") },
+                dafs::Endpoint {
+                    dafs::Address{"3.0.0.0", 300},
+                    dafs::Address{"3.0.0.1", 301},
+                    dafs::Identity("33333333-3333-3333-3333-333333333333") },
+            }
+        )
+    };
+
+    EXPECT_CALL(*ping, Start()).Times(1);
+
+    dafs::ReplicatedPartition partition(
+        std::unique_ptr<MockReplication>(new MockReplication()),
+        storage,
+        std::unique_ptr<MockNodeSet>(new MockNodeSet()),
+        std::move(ping),
+        std::shared_ptr<MockLock>(new MockLock())
+    );
+
+    EXPECT_CALL(*storage, ReadBlock(info))
+        .Times(1)
+        .WillOnce(testing::Return(format));
+
+    auto details = partition.GetNodeSetDetails();
+
+    ASSERT_EQ("11111111-1111-1111-1111-111111111111", details.minus.identity.id);
+    ASSERT_EQ("1.0.0.0", details.minus.management.ip);
+    ASSERT_EQ(100, details.minus.management.port);
+    ASSERT_EQ("1.0.0.1", details.minus.replication.ip);
+    ASSERT_EQ(101, details.minus.replication.port);
+    ASSERT_EQ("22222222-2222-2222-2222-222222222222", details.zero.identity.id);
+    ASSERT_EQ("2.0.0.0", details.zero.management.ip);
+    ASSERT_EQ(200, details.zero.management.port);
+    ASSERT_EQ("2.0.0.1", details.zero.replication.ip);
+    ASSERT_EQ(201, details.zero.replication.port);
+    ASSERT_EQ("33333333-3333-3333-3333-333333333333", details.plus.identity.id);
+    ASSERT_EQ("3.0.0.0", details.plus.management.ip);
+    ASSERT_EQ(300, details.plus.management.port);
+    ASSERT_EQ("3.0.0.1", details.plus.replication.ip);
+    ASSERT_EQ(301, details.plus.replication.port);
+}
