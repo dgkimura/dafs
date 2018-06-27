3d6cfc5980095055b8b91b3b42df920804375c02
David Kimura <dkimura@pivotal.io>
2018-06-26 17:36:21 -0700

    Wrap serialize/deserialize around protobuf interface

--- src/serialization.cpp
+++ src/serialization.cpp
@@ -1,5 +1,6 @@
 #include <map>
 
+#include <boost/lexical_cast.hpp>
 #include <boost/uuid/uuid.hpp>
 
 #include "message.pb.h"
@@ -35,6 +36,15 @@ namespace dafs
     }
 
 
+    static dafs::Identity
+    convert(const dafs::proto::Identity& obj)
+    {
+        dafs::Identity _obj;
+        _obj.id = obj.id();
+        return _obj;
+    }
+
+
     static dafs::proto::BlockFormat
     convert(const dafs::BlockFormat& obj)
     {
@@ -44,15 +54,35 @@ namespace dafs
     }
 
 
+    static dafs::BlockFormat
+    convert(const dafs::proto::BlockFormat& obj)
+    {
+        dafs::BlockFormat _obj;
+        _obj.contents = obj.contents();
+        return _obj;
+    }
+
+
     static dafs::proto::BlockInfo
     convert(const dafs::BlockInfo& obj)
     {
-        dafs::proto::Identity _obj_id = convert(obj.identity);
-
         dafs::proto::BlockInfo _obj;
         _obj.set_path(obj.path);
-        _obj.set_allocated_identity(&_obj_id);
+        *_obj.mutable_identity() = convert(obj.identity);
         _obj.set_revision(obj.revision);
+        return _obj;
+    }
+
+
+    static dafs::BlockInfo
+    convert(const dafs::proto::BlockInfo& obj)
+    {
+        dafs::Identity _obj_id = convert(obj.identity());
+
+        dafs::BlockInfo _obj;
+        _obj.path = obj.path();
+        _obj.identity = _obj_id;
+        _obj.revision = obj.revision();
         return _obj;
     }
 
@@ -67,17 +97,34 @@ namespace dafs
     }
 
 
+    static dafs::Address
+    convert(const dafs::proto::Address& obj)
+    {
+        dafs::Address _obj;
+        _obj.ip = obj.ip();
+        _obj.port = obj.port();
+        return _obj;
+    }
+
+
     static dafs::proto::Endpoint
     convert(const dafs::Endpoint& obj)
     {
-        dafs::proto::Identity _obj_id = convert(obj.identity);
-        dafs::proto::Address _obj_management = convert(obj.management);
-        dafs::proto::Address _obj_replication = convert(obj.replication);
-
         dafs::proto::Endpoint _obj;
-        _obj.set_allocated_management(&_obj_management);
-        _obj.set_allocated_replication(&_obj_replication);
-        _obj.set_allocated_identity(&_obj_id);
+        *_obj.mutable_management() = convert(obj.management);
+        *_obj.mutable_replication() = convert(obj.replication);
+        *_obj.mutable_identity() = convert(obj.identity);
+        return _obj;
+    }
+
+
+    static dafs::Endpoint
+    convert(const dafs::proto::Endpoint& obj)
+    {
+        dafs::Endpoint _obj;
+        _obj.management = convert(obj.management());
+        _obj.replication = convert(obj.replication());
+        _obj.identity = convert(obj.identity());
         return _obj;
     }
 
@@ -85,14 +132,21 @@ namespace dafs
     static dafs::proto::ReplicatedEndpoints
     convert(const dafs::ReplicatedEndpoints& obj)
     {
-        dafs::proto::Endpoint _obj_minus = convert(obj.minus);
-        dafs::proto::Endpoint _obj_zero = convert(obj.zero);
-        dafs::proto::Endpoint _obj_plus = convert(obj.plus);
-
         dafs::proto::ReplicatedEndpoints _obj;
-        _obj.set_allocated_minus(&_obj_minus);
-        _obj.set_allocated_zero(&_obj_zero);
-        _obj.set_allocated_plus(&_obj_plus);
+        *_obj.mutable_minus() = convert(obj.minus);
+        *_obj.mutable_zero() = convert(obj.zero);
+        *_obj.mutable_plus() = convert(obj.plus);
+        return _obj;
+    }
+
+
+    static dafs::ReplicatedEndpoints
+    convert(const dafs::proto::ReplicatedEndpoints& obj)
+    {
+        dafs::ReplicatedEndpoints _obj;
+        _obj.minus = convert(obj.minus());
+        _obj.zero = convert(obj.zero());
+        _obj.plus = convert(obj.plus());
         return _obj;
     }
 
@@ -110,6 +164,48 @@ namespace dafs
         _obj.set_type(mtype_map[obj.type]);
         _obj.set_content(obj.content);
         _obj.set_uuid(boost::uuids::to_string(obj.uuid));
+        return _obj;
+    }
+
+
+    static dafs::Proposal
+    convert(const dafs::proto::Proposal& obj)
+    {
+        std::map<dafs::proto::Proposal::ProposalType, dafs::ProposalType> mtype_map =
+        {
+            { dafs::proto::Proposal::WRITE_BLOCK, dafs::ProposalType::WriteBlock  },
+            { dafs::proto::Proposal::DELETE_BLOCK, dafs::ProposalType::DeleteBlock  },
+            { dafs::proto::Proposal::PING, dafs::ProposalType::Ping  }
+        };
+        dafs::Proposal _obj;
+        _obj.type = mtype_map[obj.type()];
+        _obj.content = obj.content();
+        _obj.uuid = boost::lexical_cast<boost::uuids::uuid>(obj.uuid());
+        return _obj;
+    }
+
+
+    static dafs::proto::BlockIndex
+    convert(const dafs::BlockIndex& obj)
+    {
+        dafs::proto::BlockIndex _obj;
+        for (auto& block_info : obj.items)
+        {
+            auto *o = _obj.add_items();
+            *o = convert(block_info);
+        }
+        return _obj;
+    }
+
+
+    static dafs::BlockIndex
+    convert(const dafs::proto::BlockIndex& obj)
+    {
+        dafs::BlockIndex _obj;
+        for (auto& block_info : obj.items())
+        {
+            _obj.items.push_back(convert(block_info));
+        }
         return _obj;
     }
 
@@ -135,6 +231,31 @@ namespace dafs
         dafs::proto::MetaData _obj;
         _obj.set_type(mtype_map[obj.key]);
         _obj.set_value(obj.value);
+        return _obj;
+    }
+
+
+    static dafs::MetaData
+    convert(dafs::proto::MetaData obj)
+    {
+        std::map<dafs::proto::MetaData::MetaDataType, std::string> mtype_map =
+        {
+            { dafs::proto::MetaData::BLOCK_INFO, "BlockInfo" },
+            { dafs::proto::MetaData::BLOCK_FORMAT, "BlockFormat" },
+            { dafs::proto::MetaData::ADDRESS, "Address" },
+            { dafs::proto::MetaData::ENDPOINT, "Endpoint" },
+            { dafs::proto::MetaData::IDENTIFIER, "Identity" },
+            { dafs::proto::MetaData::NODE_ENDPOINTS, "NodeEndpoints" },
+            { dafs::proto::MetaData::MINUS_REPLICATED_ENDPOINTS, "MinusReplicatedEndpoints" },
+            { dafs::proto::MetaData::ZERO_REPLICATED_ENDPOINTS, "ZeroReplicatedEndpoints" },
+            { dafs::proto::MetaData::PLUS_REPLICATED_ENDPOINTS, "PlusReplicatedEndpoints" },
+            { dafs::proto::MetaData::MINUS_IDENTITY, "MinusIdentity" },
+            { dafs::proto::MetaData::ZERO_IDENTITY, "ZeroIdentity" },
+            { dafs::proto::MetaData::PLUS_IDENTITY, "PlusIdentity" }
+        };
+        dafs::MetaData _obj;
+        _obj.key = mtype_map[obj.type()];
+        _obj.value = obj.value();
         return _obj;
     }
 
@@ -190,6 +311,177 @@ namespace dafs
     dafs::Delta deserialize(std::string obj)
     {
         dafs::proto::Delta _obj;
+        _obj.ParseFromString(obj);
+        return convert(_obj);
+    }
+
+
+    template <>
+    std::string serialize(const dafs::Identity& obj)
+    {
+        std::string data;
+        auto _obj = convert(obj);
+        _obj.SerializeToString(&data);
+        return data;
+    }
+
+
+    template <>
+    dafs::Identity deserialize(std::string obj)
+    {
+        dafs::proto::Identity _obj;
+        _obj.ParseFromString(obj);
+        return convert(_obj);
+    }
+
+
+    template <>
+    std::string serialize(const dafs::BlockFormat& obj)
+    {
+        std::string data;
+        auto _obj = convert(obj);
+        _obj.SerializeToString(&data);
+        return data;
+    }
+
+
+    template <>
+    dafs::BlockFormat deserialize(std::string obj)
+    {
+        dafs::proto::BlockFormat _obj;
+        _obj.ParseFromString(obj);
+        return convert(_obj);
+    }
+
+
+    template <>
+    std::string serialize(const dafs::BlockInfo& obj)
+    {
+        std::string data;
+        auto _obj = convert(obj);
+        _obj.SerializeToString(&data);
+        return data;
+    }
+
+
+    template <>
+    dafs::BlockInfo deserialize(std::string obj)
+    {
+        dafs::proto::BlockInfo _obj;
+        _obj.ParseFromString(obj);
+        return convert(_obj);
+    }
+
+
+    template <>
+    std::string serialize(const dafs::Address& obj)
+    {
+        std::string data;
+        auto _obj = convert(obj);
+        _obj.SerializeToString(&data);
+        return data;
+    }
+
+
+    template <>
+    dafs::Address deserialize(std::string obj)
+    {
+        dafs::proto::Address _obj;
+        _obj.ParseFromString(obj);
+        return convert(_obj);
+    }
+
+
+    template <>
+    std::string serialize(const dafs::Endpoint& obj)
+    {
+        std::string data;
+        auto _obj = convert(obj);
+        _obj.SerializeToString(&data);
+        return data;
+    }
+
+
+    template <>
+    dafs::Endpoint deserialize(std::string obj)
+    {
+        dafs::proto::Endpoint _obj;
+        _obj.ParseFromString(obj);
+        return convert(_obj);
+    }
+
+
+    template <>
+    std::string serialize(const dafs::ReplicatedEndpoints& obj)
+    {
+        std::string data;
+        auto _obj = convert(obj);
+        _obj.SerializeToString(&data);
+        return data;
+    }
+
+
+    template <>
+    dafs::ReplicatedEndpoints deserialize(std::string obj)
+    {
+        dafs::proto::ReplicatedEndpoints _obj;
+        _obj.ParseFromString(obj);
+        return convert(_obj);
+    }
+
+
+    template <>
+    std::string serialize(const dafs::Proposal& obj)
+    {
+        std::string data;
+        auto _obj = convert(obj);
+        _obj.SerializeToString(&data);
+        return data;
+    }
+
+
+    template <>
+    dafs::Proposal deserialize(std::string obj)
+    {
+        dafs::proto::Proposal _obj;
+        _obj.ParseFromString(obj);
+        return convert(_obj);
+    }
+
+
+    template <>
+    std::string serialize(const dafs::BlockIndex& obj)
+    {
+        std::string data;
+        auto _obj = convert(obj);
+        _obj.SerializeToString(&data);
+        return data;
+    }
+
+
+    template <>
+    dafs::BlockIndex deserialize(std::string obj)
+    {
+        dafs::proto::BlockIndex _obj;
+        _obj.ParseFromString(obj);
+        return convert(_obj);
+    }
+
+
+    template <>
+    std::string serialize(const dafs::MetaData& obj)
+    {
+        std::string data;
+        auto _obj = convert(obj);
+        _obj.SerializeToString(&data);
+        return data;
+    }
+
+
+    template <>
+    dafs::MetaData deserialize(std::string obj)
+    {
+        dafs::proto::MetaData _obj;
         _obj.ParseFromString(obj);
         return convert(_obj);
     }
--- unittests/serialization_unittest.cpp
+++ unittests/serialization_unittest.cpp
@@ -219,3 +219,229 @@ TEST(SerializationUnitTest, _testDeltaIsSerializableAndDeserializable)
 
     ASSERT_EQ(expected.difference, actual.difference);
 }
+
+
+TEST(SerializationUnitTest, _testIdentityIsSerializableAndDeserializable)
+{
+    dafs::Identity expected("33333333-3333-3333-3333-333333333333");
+
+    std::string string_obj = dafs::serialize(expected);
+    dafs::Identity actual = dafs::deserialize<dafs::Identity>(string_obj);
+
+    ASSERT_EQ(expected.id, actual.id);
+}
+
+
+TEST(SerializationUnitTest, _testBlockFormatIsSerializableAndDeserializable)
+{
+    dafs::BlockFormat expected
+    {
+        "this is the block contents"
+    }, actual;
+
+    std::string string_obj = dafs::serialize(expected);
+    actual = dafs::deserialize<dafs::BlockFormat>(string_obj);
+
+    ASSERT_EQ(std::string(expected.contents), std::string(actual.contents));
+}
+
+
+TEST(SerializationUnitTest, _testBlockInfoIsSerializableAndDeserializable)
+{
+    dafs::BlockInfo expected
+    {
+        "the-path",
+        dafs::Identity
+        {
+            "00000000-0000-0000-0000-000000000000",
+        },
+        23  //revision
+    }, actual;
+
+    std::string string_obj = dafs::serialize(expected);
+    actual = dafs::deserialize<dafs::BlockInfo>(string_obj);
+
+    ASSERT_EQ(expected.path, actual.path);
+    ASSERT_EQ(expected.identity, actual.identity);
+}
+
+
+TEST(SerializationUnitTest, _testMessageIsSerializableAndDeserializable)
+{
+    dafs::Address expected
+    {
+        "the-ip-address",
+        8080
+    }, actual;
+
+    std::string string_obj = dafs::serialize(expected);
+    actual = dafs::deserialize<dafs::Address>(string_obj);
+
+    ASSERT_EQ(std::string(expected.ip), std::string(actual.ip));
+    ASSERT_EQ(expected.port, actual.port);
+}
+
+
+TEST(SerializationUnitTest, _testEndpointIsSerializableAndDeserializable)
+{
+    dafs::Endpoint expected
+    {
+        dafs::Address
+        {
+            "1.1.1.1", 1111
+        },
+        dafs::Address
+        {
+            "2.2.2.2", 2222
+        },
+        dafs::Identity("33333333-3333-3333-3333-333333333333")
+
+    }, actual;
+
+    std::string string_obj = dafs::serialize(expected);
+    actual = dafs::deserialize<dafs::Endpoint>(string_obj);
+
+    ASSERT_EQ(expected.management.ip, actual.management.ip);
+    ASSERT_EQ(expected.management.port, actual.management.port);
+    ASSERT_EQ(expected.replication.ip, actual.replication.ip);
+    ASSERT_EQ(expected.replication.port, actual.replication.port);
+    ASSERT_EQ(expected.identity, actual.identity);
+}
+
+
+TEST(SerializationUnitTest, _testReplicatedEndpointsIsSerializableAndDeserializable)
+{
+    dafs::ReplicatedEndpoints expected
+    {
+        dafs::Endpoint
+        {
+            dafs::Address
+            {
+                "1.1.1.1", 1111
+            },
+            dafs::Address
+            {
+                "2.2.2.2", 2222
+            },
+            dafs::Identity("11111111-1111-1111-1111-111111111111")
+
+        },
+        dafs::Endpoint
+        {
+            dafs::Address
+            {
+                "3.3.3.3", 3333
+            },
+            dafs::Address
+            {
+                "4.4.4.4", 4444
+            },
+            dafs::Identity("22222222-2222-2222-2222-222222222222")
+
+        },
+        dafs::Endpoint
+        {
+            dafs::Address
+            {
+                "5.5.5.5", 5555
+            },
+            dafs::Address
+            {
+                "6.6.6.6", 6666
+            },
+            dafs::Identity("33333333-3333-3333-3333-333333333333")
+
+        },
+    }, actual;
+
+    std::string string_obj = dafs::serialize(expected);
+    actual = dafs::deserialize<dafs::ReplicatedEndpoints>(string_obj);
+
+    ASSERT_EQ(expected.minus.management.ip, actual.minus.management.ip);
+    ASSERT_EQ(expected.minus.management.port, actual.minus.management.port);
+    ASSERT_EQ(expected.minus.replication.ip, actual.minus.replication.ip);
+    ASSERT_EQ(expected.minus.replication.port, actual.minus.replication.port);
+    ASSERT_EQ(expected.minus.identity, actual.minus.identity);
+
+    ASSERT_EQ(expected.zero.management.ip, actual.zero.management.ip);
+    ASSERT_EQ(expected.zero.management.port, actual.zero.management.port);
+    ASSERT_EQ(expected.zero.replication.ip, actual.zero.replication.ip);
+    ASSERT_EQ(expected.zero.replication.port, actual.zero.replication.port);
+    ASSERT_EQ(expected.zero.identity, actual.zero.identity);
+
+    ASSERT_EQ(expected.plus.management.ip, actual.plus.management.ip);
+    ASSERT_EQ(expected.plus.management.port, actual.plus.management.port);
+    ASSERT_EQ(expected.plus.replication.ip, actual.plus.replication.ip);
+    ASSERT_EQ(expected.plus.replication.port, actual.plus.replication.port);
+    ASSERT_EQ(expected.plus.identity, actual.plus.identity);
+}
+
+
+TEST(SerializationUnitTest, _testProposalIsSerializableAndDeserializable)
+{
+    boost::uuids::string_generator gen;
+    dafs::Proposal expected
+    {
+        dafs::ProposalType::Ping,
+        "the proposal content",
+        gen("{11111111-2222-3333-4444-555555555555}")
+    }, actual;
+
+    std::string string_obj = dafs::serialize(expected);
+    actual = dafs::deserialize<dafs::Proposal>(string_obj);
+
+    ASSERT_EQ(expected.type, actual.type);
+    ASSERT_EQ(expected.content, actual.content);
+    ASSERT_EQ(expected.uuid, actual.uuid);
+}
+
+
+TEST(SerializationUnitTest, _testBlockIndexIsSerializableAndDeserializable)
+{
+    dafs::BlockIndex expected
+    {
+        std::vector<dafs::BlockInfo>
+        {
+            dafs::BlockInfo
+            {
+                "my-block",
+                dafs::Identity
+                {
+                    "00000000-0000-0000-0000-000000000000",
+                },
+                0  // revision
+            },
+            dafs::BlockInfo
+            {
+                "your-block",
+                dafs::Identity
+                {
+                    "11111111-1111-1111-1111-111111111111",
+                },
+                1  // revision
+            }
+        }
+    }, actual;
+
+    std::string string_obj = dafs::serialize(expected);
+    actual = dafs::deserialize<dafs::Index<dafs::BlockInfo>>(string_obj);
+
+    ASSERT_EQ(expected.items[0].path, actual.items[0].path);
+    ASSERT_EQ(expected.items[1].identity, actual.items[1].identity);
+}
+
+
+TEST(SerializationUnitTest, _testMetaDataIsSerializableAndDeserializable)
+{
+    dafs::MetaData expected
+    {
+        "Address", // must be valid metadata key
+        "value-string"
+    }, actual;
+
+    std::string string_obj = dafs::serialize(expected);
+    actual = dafs::deserialize<dafs::MetaData>(string_obj);
+
+    ASSERT_EQ(expected.key, actual.key);
+    ASSERT_EQ(expected.value, actual.value);
+}
